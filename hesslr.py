64acbaeb2389232ec2299e6f7233a345b6637167
hellolsk <44597193+hellolsk@users.noreply.github.com>
2023-01-27 20:18:30 +0800

    Update hesslr.py

--- hesslr.py
+++ hesslr.py
@@ -18,7 +18,6 @@ def guiyihua(data):
     _range = np.max(data) - np.min(data)
     return (data - np.min(data)) / _range
 
-# 加载数据
 def load_data(file_name):
     df = pd.read_csv(file_name)
     # diabetes 8*features
@@ -33,25 +32,22 @@ def load_data(file_name):
 
     # print("fg:",fg)
     ones = np.ones(shape=fg.shape[0])
-    # np.c_按行链接矩阵
+
     fg = np.c_[fg, ones]
     # print("features:", features[0])
     # print('fixed features shape: ', features_g.shape)
 
-    # 随机划分训练集和测试集
     fg_train,fg_test,fh_train,fh_test=train_test_split(fg,fh,test_size=0.3,random_state=1)
     
-    # labels minst
     labels = np.squeeze(df.iloc[:, -1].to_numpy().reshape(1, -1))
     # labels = np.squeeze(df.iloc[:, -1:].to_numpy().reshape(1, -1))
     # labels = normalization(labels)
-    # 变为1和-1
     # labels = labels*2-1
     labels_train,labels_test = train_test_split(labels,test_size=0.3,random_state=1)
     # print('labels shape: ', labels.shape)
     return fg_test,fg_train, fh_test,fh_train, labels_test,labels_train
 
-# 批量读取数据
+
 def data_iter(batch_size, x1, x2, y):
     num_examples = len(y)
     indices = list(range(num_examples))
@@ -65,14 +61,11 @@ def data_iter(batch_size, x1, x2, y):
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
-# loss计算没变，还是baseline
+
 def compute_loss(X1,X2, y, w11,w12,w21,w22):
-    # 这里同ihvlr一样仍然是明文计算loss
     # w1 = w11+w12
     # w2 = w21+w22
     z = np.dot(X1,w11)+np.dot(X1,w12)+np.dot(X2,w21)+np.dot(X2,w22)
-    # z = np.dot(X1,w1)+np.dot(X2,w2)
-    # print("-----------z:",z)
     # y_hat = 0.5+0.125*z
     y_hat = sigmoid(z)
     # print("y_hat:",y_hat)
@@ -83,34 +76,29 @@ def compute_loss(X1,X2, y, w11,w12,w21,w22):
     return loss
 # x.shape=(batch,n)
 def compute_gradient(pk,sk,X1,X2, y, w11,w12,w21,w22):
-    # A
     z11 = np.dot(X1,w11)
     # print("w11",w11)
     # print("w12",w12)
     # print("w21",w21)
     # print("w22",w22)
 
-    # print("---z11---",z11)
     z121,z122 = ssm.ssm(pk,sk,X1,w12)
-    # print("----------z121---",z121)
-    # print("--------------------z122---",z122)
 
-    # B
+
     z22 = np.dot(X2,w22)
     z211,z212 = ssm.ssm(pk,sk,X2,w21)
-    # A
+
     z1 = z11+z121+z211
-    # print("*****z1",z1)
+
     z1_e = np.asarray([pk.encrypt(m) for m in z1])
     # z1_e2 = np.asarray([pk.encrypt(m) for m in z1*z1])
     # z1_e3 = np.asarray([pk.encrypt(m) for m in z1*z1*z1])
-    # B
+
     z2 = z22+z122+z212
     z = z1_e+z2
     # print("------z2------",z2)
     # print("-----z------",z)
     # z_3e = z1_e3+3*z1_e2*z2+3*z1_e*z2*z2+z2*z2*z2
-    # 密文的形式
     # y_hat = 0.5+0.197*z-0.004*z_3e
     y_hat = 0.5+0.125*z
     e = y_hat-y
@@ -133,21 +121,20 @@ def compute_gradient(pk,sk,X1,X2, y, w11,w12,w21,w22):
     return g11/n,g121/n,g122/n,g21/n,g22/n
 
 
-# 训练
+
 def fit(X1,X2, y,fg_test,fh_test,labels_test):
     print('fit start')
-    # 初始化模型参数
     np.random.seed(1)
     losslist=[]
     acclist=[]
     auclist=[]
     w1 = np.ones(X1.shape[1])
     w2 = np.ones(X2.shape[1])
-    # 拆分w
+
     w11,w12 = ass.asslist(w1,len(w1))
     w21,w22 = ass.asslist(w2,len(w2))
     pk,sk = paillier.generate_paillier_keypair(n_length=1024)
-    # 开始训练
+
     batch_size = 64
     learning_rate = 0.1
     iter_max = 30
@@ -186,7 +173,7 @@ def fit(X1,X2, y,fg_test,fh_test,labels_test):
     return w11,w12,w21,w22,losslist,acclist,auclist
 
 
-# 预测
+
 def predict(X1,X2, y, w11,w12,w21,w22):
     count = 0
     w1 = w11+w12
@@ -204,7 +191,7 @@ if __name__ == '__main__':
     w11,w12,w21,w22,losslist,acclist,auclist = fit(fg_train, fh_train,labels_train,fg_test,fh_test,labels_test)
     print("w1:",w11+w12)
     print("w2:",w21+w22)
-    print(f'耗时：{time.time()-t1:.3f}s')
+    print(f'cost：{time.time()-t1:.3f}s')
     # predict_result = predict(fg_test, fh_test,labels_test, w1,w2)
     # print(f'predict_result: {predict_result}%')
 
